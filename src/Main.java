e9ceb8635b5a076cf7ba8930441adeaa10d1873a
Behnam Nazarpak <behnamnazarpak97@gmail.com>
2017-02-01 15:41:48 +0330

    init commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I'm Success");
+    }
+
+    public static int sumNumber(int a , int b) {
+        return a + b;
+    }
+
+    public static int subNumber(int a , int b) {
+        return a - b;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Behnam on 28/01/2017.
+ */
+public class Test {
+}
